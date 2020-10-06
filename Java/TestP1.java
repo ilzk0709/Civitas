054b5f787ee79741fbd53b763b4692fd8f2a3b3e
Roberro <46483421+ilzk0709@users.noreply.github.com>
2020-10-06 11:00:04 +0200

    Terminado TestP1

--- Java/TestP1.java
+++ Java/TestP1.java
@@ -16,22 +16,30 @@ public class TestP1 {
     }
     
     public static void main (String args[]) {
-        boolean funciona = true;
-        int resultado = 0, suma = 0;
-        int iter = 24;
-        double probabilidades[] = {1,2,3,4};
         
+        boolean funciona = true;
+        int resultado = 0, suma = 0, carcel = 7, casillas = 20, posicion = 0;
+        int iter = 100;
+        double probabilidades[] = {0,0,0,0};
+        Sorpresa s = new Sorpresa(), s1 = new Sorpresa();
+        MazoSorpresas mazo = new MazoSorpresas();
+        Tablero tablero = new Tablero(carcel);
         
         
         for (int i = 0; i < iter; i++) {
             resultado = Dado.getInstance().quienEmpieza(probabilidades.length);
+            //Los valores van del 0 al 3 porque son los índices del vector
+            //de probabilidades
             probabilidades[resultado]++;
         }
+        
+        System.out.println("Veces: " + probabilidades.toString());
+        
         for (int i = 0; i < probabilidades.length; i++) {
             probabilidades[i] = probabilidades[i]/iter;
         }
         
-        
+        System.out.println("Probabilidades: " + probabilidades.toString());
         
         Dado.getInstance().setDebug(true);
         for (int i = 0; i < iter; i++) {
@@ -52,8 +60,39 @@ public class TestP1 {
         }
         if (suma == iter)
                 funciona = false;
+        
+    //*******************Funciona************************
     
-        System.out.println(Dado.getInstance().salgoDeLaCarcel());
-        System.out.println(Dado.getInstance().getUltimoResultado());
+        if (Dado.getInstance().salgoDeLaCarcel())
+            System.out.println("Sales de la cárcel");
+        else if (!Dado.getInstance().salgoDeLaCarcel())
+            System.out.println("No sales de la cárcel");
+        else
+            System.err.println("Dado.salgoDeLaCarcel no funciona");
+        
+        System.out.println("El último resltado ha sido " +
+                Dado.getInstance().getUltimoResultado());
+        
+        mazo.alMazo(s);
+        mazo.alMazo(s1);
+        mazo.siguiente();
+        mazo.inhabilitarCartaEspecial(s1);
+        mazo.habilitarCartaEspecial(s1);
+        while (Diario.getInstance().eventosPendientes())
+            System.out.println(Diario.getInstance().leerEvento());
+        
+        for (int i = 0; i < casillas; i++) {
+            tablero.aniadeCasilla(new Casilla());
+        }
+        
+        tablero.vaciar();
+        
+        for (int i = 0; i < carcel - 2; i++) {
+            tablero.aniadeCasilla(new Casilla());
+        }
+        
+        posicion = tablero.nuevaPosicion(posicion, Dado.getInstance().tirar());
+        if (posicion == -1)
+            System.err.println("Tablero incorrecto");
     }
 }
